48c467d73cf5d96878ccb11e1002dd5c5b36d9f8
zhangaonanxyz-ui <zhangaonanxyz@gmail.com>
2026-07-15 11:09:33 +0800

    chore: bump version to 1.3 (versionCode 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.nuwa.skillchat"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         vectorDrawables {
             useSupportLibrary = true
